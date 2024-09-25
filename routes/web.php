2a59362d88ac6cf2f0cb8e249f80a7da681046f4
Aseef Zainal <aseefzainal65@gmail.com>
2024-09-25 09:38:14 -0700

    Set up Laravel Jetstream with Livewire and dark mode support
    - Installed Tailwind CSS for styling
    
    - Integrated Flowbite for UI components

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('user-list');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
